04027d84782adefc06440f637db23595d322c09d
✬Ravindu Manoj✬ ✓ <88134633+ravindu01manoj@users.noreply.github.com>
2021-10-24 15:32:12 +0530

    Update sew.js

--- sew.js
+++ sew.js
@@ -75,6 +75,7 @@ Array.prototype.remove = function () {
         return this;
 };
 async function sewQueen() {
+    console.log('test')
         await Details.DATABASE.sync();
         var StrSes_Db = await SewQueenDB.findAll({
                 where: {
@@ -87,7 +88,27 @@ async function sewQueen() {
         await sendMessageownerMSG(DataKey)
         await sendMessageBIOMSG(DataKey)
         await sendMessageADSMSG(DataKey)
-        DataKey.logger.level = Details.DEBUG ? 'debug' : 'warn';
+     //   DataKey.logger.level = Details.DEBUG ? 'debug' : 'warn';
+ 
+    var logger_levels = ''
+    if (Details.DEBUG == 'true') {
+        logger_levels = 'all'
+    } else if (Details.DEBUG == 'false') {
+        logger_levels = 'off'
+    } else if (Details.DEBUG == 'trace') {
+        logger_levels = 'trace'
+    } else if (Details.DEBUG == 'fatal') {
+        logger_levels = 'fatal'
+    } else if (Details.DEBUG == 'warn') {
+        logger_levels = 'warn'
+    } else if (Details.DEBUG == 'error') {
+        logger_levels = 'error'
+    } else if (Details.DEBUG == 'info') {
+        logger_levels = 'info'
+    } else {
+        logger_levels = 'warn'
+    }
+    DataKey.logger.level = logger_levels
         var Lostdb;
         if (StrSes_Db.length < 1) {
                 Lostdb = true;
