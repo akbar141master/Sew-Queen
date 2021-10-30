24a875ada1026ccffca4d3176ea9e1e172e3aab8
✬Ravindu Manoj✬ ✓ <88134633+ravindu01manoj@users.noreply.github.com>
2021-10-30 17:37:04 +0530

    Create fulldata.js

---
+++ fulldata.js
@@ -0,0 +1,5 @@
+module.exports = {
+   SEW_QUEEN_OWNER: '💖', // Ravindu Manoj - Sew Queen Owner
+   SEW_QUEEN_TEAM: '💖', // Team - SEW QUEEN TEAM 
+   QUEEN_AMDI_OWNER: '94757405652' // Black Amda - Queen Amdi Owner --> For The Friendship 
+    } 
